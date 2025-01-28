1dc362e526748639504f8b2fcc6e997f7f40e995
Minjun (Dominic) Jung <m48497943@gmail.com>
2025-01-29 08:26:34 +1100

    Delete BigInt (Barrett) 1.0 directory

--- BigInt (Barrett) 1.0/make_bigint.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem gcc 5.1.0 (tdm64) win10
-g++ bigint.cc -O3 -std=c++11 -Wall -Wextra -Wpedantic -pedantic -DBUILD_LIB -shared -o bigint.dll
-pause
--- BigInt (Barrett) 1.0/make_main.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem gcc 5.1.0 (tdm64) win10
-g++ main.cc -O3 -std=c++11 -Wall -Wextra -Wpedantic -pedantic -L./ -lbigint -o pow0x.exe
-pause
